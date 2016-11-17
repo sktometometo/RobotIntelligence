856b561c3e0d019dbecbb221ebae4085b462dbd6
sktometometo <sktometometo@gmail.com>
2016-11-18 00:26:02 +0900

    first commit.

---
+++ NN.py
@@ -0,0 +1,100 @@
+#:coding=utf-8:
+
+import numpy as np
+import random
+import struct
+
+def sigmoid(a,z):
+    return 1/(1+np.exp(-a*z))
+
+def sigmoid_d(a,z):
+    return a*sigmoid(a,z)*(1-sigmoid(a,z))
+
+class nn:
+    def __init__(self,lst_unitnum,a,learning_rate):
+        self.a = a
+        self.learning_rate = learning_rate
+        self.layer_num = len(lst_unitnum)
+        self.weightslst = []
+        for i in range(0,self.layer_num-1):
+            self.weightslst.append(np.ones((lst_unitnum[i+1],lst_unitnum[i]+1)))
+
+    def fit(self,X,Y):
+        test_num = len(Y)
+        Indlst = np.random.randint(0,test_num-1,test_num-1)
+        for i in Indlst:
+            for j in range(0,100):
+                outputs = []
+                outputs.append(X[i]) # 入力層の出力
+                for k in range(0,self.layer_num-1):
+                    outputs.append(sigmoid(self.a,np.dot(self.weightslst[k],np.append(outputs[-1],1))))
+                    # 各層の出力を計算してい
+
+                # 誤差逆伝播法
+                if np.linalg.norm(Y[i]-outputs[-1]) < 0.00001:
+                    break
+                #delta = (Y[i]-outputs[-1])*self.a*outputs[-1]*(np.ones(len(outputs[-1]))-outputs[-1])
+                #print(delta.size)
+                self.weightslst[-1] = self.weightslst[-1] - self.learning_rate * np.array(np.transpose(np.mat(Y[i]-outputs[-1])) * np.mat(np.append(outputs[-2],1)))
+                for k in range(1,self.layer_num-2):
+                    delta = np.dot(self.weightslst[-1-k],delta) * (self.a*outputs[-1-k]*(np.ones(len(outputs[-1-k]))-outputs[-1-k]))
+                    self.weightslst[-1-k] = self.weightslst[-1-k] - self.learning_rate * np.transpose(np.mat(delta)) * np.mat(np.append(outputs[-1-k],1))
+
+    def predict_one(self,xi):
+        output = xi
+        for i in range(0,self.layer_num):
+            output = sigmoid(self.a,np.dot(self.weightslst[i],np.append(output,1)))
+        return output
+
+    def predict(self,X):
+        outputs = []
+        ItemNum = len(X)
+        for i in range(0,ItemNum):
+            outputs.append(self.predict_one(X[i]))
+        return outputs
+
+def Xread(fname):
+    f_x = open(fname,'rb')
+    X = []
+    f_x.read(4);
+    ItemNum = struct.unpack('>i',f_x.read(4))[0];
+    Num_row = struct.unpack('>i',f_x.read(4))[0];
+    Num_col = struct.unpack('>i',f_x.read(4))[0];
+    buf = np.ones((Num_row*Num_col))
+    for i in range(0,ItemNum):
+        for j in range(0,Num_row*Num_col):
+            buf[j] = struct.unpack('B',f_x.read(1))[0]
+        X.append(buf)
+    return X
+
+def Yread(fname):
+    f_y = open(fname,'rb')
+    Y = []
+    f_y.read(4)
+    ItemNum = struct.unpack('>i',f_y.read(4))[0]
+    for i in range(0,ItemNum):
+        buf = np.zeros((10))
+        buf[struct.unpack('B',f_y.read(1))[0]] = 1
+        Y.append(buf)
+    return Y
+
+if __name__ == "__main__":
+    train_x = Xread('MNIST/train-images-idx3-ubyte')
+    train_y = Yread('MNIST/train-labels-idx1-ubyte')
+    test_x = Xread('MNIST/t10k-images-idx3-ubyte')
+    test_y = Yread('MNIST/t10k-labels-idx1-ubyte')
+
+    lst_unitnum = [28*28,1000,10]
+    a = 10
+    learning_rate = 0.1
+
+    neuralnet = nn(lst_unitnum,a,learning_rate)
+    neuralnet.fit(train_x,train_y)
+
+    test_outputs = neuralnet.predict(test_x)
+    ItemNum = len(test_outputs)
+    correctnum = 0
+    for i in range(0,ItemNum):
+        if np.linalg.norm(test_outputs[i]-test_y[i]) < 0.0001:
+            correctnum = correctnum + 1
+    print("accuracy:"+str(correctnum*1.0/ItemNum))
